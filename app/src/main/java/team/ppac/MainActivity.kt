5a6694c454a16336f41d568bcfb78a150c3d8bb2
Jaesung Lee <viki0114@naver.com>
2024-04-28 01:28:07 +0900

    [Chore] 기존 Theme 제거에 따른 코드 수정

--- app/src/main/java/team/ppac/MainActivity.kt
+++ app/src/main/java/team/ppac/MainActivity.kt
@@ -3,28 +3,16 @@ package team.ppac
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import team.ppac.ui.theme.PPACTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PPACTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting("Android")
         }
     }
 }
@@ -40,7 +28,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    PPACTheme {
-        Greeting("Android")
-    }
+    Greeting("Android")
 }
