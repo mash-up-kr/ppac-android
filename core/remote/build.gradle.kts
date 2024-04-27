c093d60387794a8855e58fb6e58387fcd3fd8e3b
evergreen <tkdfhr7497@naver.com>
2024-04-28 00:08:47 +0900

    [Feat] Domain, navigation, remote, feature gradle 주석 제거

--- core/remote/build.gradle.kts
+++ core/remote/build.gradle.kts
@@ -1,38 +1,12 @@
-//plugins {
-//    alias(libs.plugins.androidLibrary)
-//    alias(libs.plugins.jetbrainsKotlinAndroid)
-//}
-//
-//android {
-//    namespace = "team.ppac.remote"
-//    compileSdk = 34
-//
-//    defaultConfig {
-//        minSdk = 26
-//
-//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_17
-//        targetCompatibility = JavaVersion.VERSION_17
-//    }
-//    kotlinOptions {
-//        jvmTarget = "17"
-//    }
-//}
-//
-//dependencies {
-//
-//    implementation(libs.androidx.core.ktx)
-//    implementation(libs.androidx.appcompat)
-//    implementation(libs.material)
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.androidx.junit)
-//    androidTestImplementation(libs.androidx.espresso.core)
-//}
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "team.ppac.remote"
+}
+
+dependencies {
+
+}
--- feature/sample/build.gradle.kts
+++ feature/sample/build.gradle.kts
@@ -1,31 +1,12 @@
-//plugins {
-//    alias(libs.plugins.androidLibrary)
-//    alias(libs.plugins.jetbrainsKotlinAndroid)
-//}
-//
-//android {
-//    namespace = "team.ppac.feature.sample"
-//    compileSdk = 34
-//
-//    defaultConfig {
-//        minSdk = 26
-//
-//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_17
-//        targetCompatibility = JavaVersion.VERSION_17
-//    }
-//    kotlinOptions {
-//        jvmTarget = "17"
-//    }
-//}
-//
-//dependencies {
-//}
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "team.ppac.feature.sample"
+}
+
+dependencies {
+
+}
