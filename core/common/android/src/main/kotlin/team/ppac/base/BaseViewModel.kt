3ba9f365da78b1f640b04735debace1bf91599ba
ricky <ricky@levvels.io>
2024-05-08 18:26:54 +0900

    [Feat] BaseViewModel 및 Intent, SideEffect, State 작성

---
+++ core/common/android/src/main/kotlin/team/ppac/base/BaseViewModel.kt
@@ -0,0 +1,42 @@
+package team.ppac.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<S : UiState, SE : UiSideEffect, I : UiIntent>(
+    initialState: S,
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(initialState)
+    val state = _uiState.asStateFlow()
+
+
+    private val _sideEffect: MutableSharedFlow<SE> = MutableSharedFlow()
+    val sideEffect = _sideEffect.asSharedFlow()
+
+    // Get current state
+    protected val currentState: S
+        get() = _uiState.value
+
+    open fun intent(intent: I) {
+        viewModelScope.launch {
+            handleIntent(intent)
+        }
+    }
+
+    protected abstract suspend fun handleIntent(intent: I)
+
+    protected fun reduce(reduce: S.() -> S) {
+        val state = currentState.reduce()
+        _uiState.value = state
+    }
+
+    protected fun sendEffect(sideEffect: SE) {
+        viewModelScope.launch { _sideEffect.emit(sideEffect) }
+    }
+}
