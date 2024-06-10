aab985c6f16ab22d0c1e9f79ceac0b7223923323
ze-zeh <chojh922@gmail.com>
2024-06-10 23:43:07 +0900

    [Feat] RepositoryModule & UseCaseModule 추가

---
+++ core/data/src/main/java/team/ppac/data/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package team.ppac.data.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import team.ppac.data.repository.SampleRepositoryImpl
+import team.ppac.domain.repository.SampleRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindSampleRepository(impl: SampleRepositoryImpl): SampleRepository
+}
