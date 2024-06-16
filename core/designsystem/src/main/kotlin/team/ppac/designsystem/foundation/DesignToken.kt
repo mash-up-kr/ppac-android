a13cad122641aed2f405d840deb22fbcb1896bdb
ze-zeh <chojh922@gmail.com>
2024-06-16 21:12:40 +0900

    [Feat] 시멘틱 컬러 추가

--- core/designsystem/src/main/kotlin/team/ppac/designsystem/foundation/DesignToken.kt
+++ core/designsystem/src/main/kotlin/team/ppac/designsystem/foundation/DesignToken.kt
@@ -3,12 +3,60 @@ package team.ppac.designsystem.foundation
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
+data class FarmemeText(
+    val primary: Color = Gray90,
+    val secondary: Color = Gray70,
+    val tertiary: Color = Gray60,
+    val assistive: Color = Gray40,
+    val disabled: Color = Gray30,
+    val inverse: Color = White,
+    val brand: Color = Orange100,
+)
+
+data class FarmemeIcon(
+    val primary: Color = Gray90,
+    val secondary: Color = Gray70,
+    val tertiary: Color = Gray60,
+    val assistive: Color = Gray40,
+    val disabled: Color = Gray30,
+    val inverse: Color = White,
+    val brand: Color = Orange100,
+)
+
 data class FarmemeBorder(
     val primary: Color = Gray90,
     val secondary: Color = Gray20,
     val assistive: Color = Gray10,
 )
 
+data class FarmemeBackground(
+    val primary: Color = Gray90,
+    val assistive: Color = Gray10,
+    val dimmer: Color = Alpha40,
+    val brand: Color = Orange100,
+)
+
+data class FarmemeSkeleton(
+    val primary: Color = Gray20,
+    val secondary: Color = Gray30,
+)
+
+val LocalFarmemeText = staticCompositionLocalOf {
+    FarmemeText()
+}
+
+val LocalFarmemeIcon = staticCompositionLocalOf {
+    FarmemeIcon()
+}
+
 val LocalFarmemeBorder = staticCompositionLocalOf {
     FarmemeBorder()
+}
+
+val LocalFarmemeBackground = staticCompositionLocalOf {
+    FarmemeBackground()
+}
+
+val LocalFarmemeSkeleton = staticCompositionLocalOf {
+    FarmemeSkeleton()
 }
--- core/designsystem/src/main/kotlin/team/ppac/designsystem/foundation/Theme.kt
+++ core/designsystem/src/main/kotlin/team/ppac/designsystem/foundation/Theme.kt
@@ -5,10 +5,30 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 
 object FarmemeTheme {
+    val text: FarmemeText
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalFarmemeText.current
+
+    val icon: FarmemeIcon
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalFarmemeIcon.current
+
     val border: FarmemeBorder
         @Composable
         @ReadOnlyComposable
         get() = LocalFarmemeBorder.current
+
+    val background: FarmemeBackground
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalFarmemeBackground.current
+
+    val skeleton: FarmemeSkeleton
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalFarmemeSkeleton.current
 
     val typography: FarmemeTypography
         @Composable
