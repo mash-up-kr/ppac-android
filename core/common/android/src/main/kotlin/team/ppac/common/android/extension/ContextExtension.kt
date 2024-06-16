28da1e95e0eb9211abbef6f406d6e533fece368e
Jaesung Lee <viki0114@naver.com>
2024-06-16 20:40:51 +0900

    [Feat] Context에서 Intent 사용 가능한 extension 추가

---
+++ core/common/android/src/main/kotlin/team/ppac/common/android/extension/ContextExtension.kt
@@ -0,0 +1,11 @@
+package team.ppac.common.android.extension
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+
+inline fun <reified T : Activity> Context.getIntent(
+    intentBuilder: Intent.() -> Intent = { this },
+): Intent {
+    return intentBuilder(Intent(this, T::class.java))
+}
