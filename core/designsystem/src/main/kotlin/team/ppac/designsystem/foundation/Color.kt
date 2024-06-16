0f31f3b242e71067d38077be5eddf807415cb681
ze-zeh <chojh922@gmail.com>
2024-06-16 19:58:41 +0900

    [Feat] 컬러 추가

--- core/designsystem/src/main/kotlin/team/ppac/designsystem/foundation/Color.kt
+++ core/designsystem/src/main/kotlin/team/ppac/designsystem/foundation/Color.kt
@@ -2,6 +2,33 @@ package team.ppac.designsystem.foundation
 
 import androidx.compose.ui.graphics.Color
 
-
+// Common
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
+
+// Neutral
+val Gray10 = Color(0xFFF2F5F7)
+val Gray20 = Color(0xFFE3E9ED)
+val Gray30 = Color(0xFFD0D8DE)
+val Gray40 = Color(0xFFBDCAD4)
+val Gray50 = Color(0xFFA5B5C1)
+val Gray60 = Color(0xFF8797A3)
+val Gray70 = Color(0xFF63717C)
+val Gray80 = Color(0xFF2E363C)
+val Gray90 = Color(0xFF1E2227)
+
+// Orange
+val Orange10 = Color(0xFFFFECE4)
+val Orange50 = Color(0xFFFFBDA0)
+val Orange100 = Color(0xFFFE573A)
+
+// Red
+val Red = Color(0xFFDD2626)
+
+// Blue
+val Blue = Color(0xFF007AFF)
+
+// Alpha
+val Alpha20 = Color(0x33000000)
+val Alpha40 = Color(0x66000000)
+val Alpha60 = Color(0x99000000)
