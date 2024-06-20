11b4eaae1cf4d88eb8fdb51c048350d20fd00d87
ze-zeh <chojh922@gmail.com>
2024-06-21 00:54:50 +0900

    [Feat] Button Sample 추가

--- feature/sample/src/main/java/team/ppac/sample/SampleScreen.kt
+++ feature/sample/src/main/java/team/ppac/sample/SampleScreen.kt
@@ -2,6 +2,7 @@ package team.ppac.sample
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -10,10 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import team.ppac.designsystem.FarmemeTheme
+import team.ppac.designsystem.component.button.FarmemeCircleButton
+import team.ppac.designsystem.component.button.FarmemeFilledButton
+import team.ppac.designsystem.component.button.FarmemeWeakButton
+import team.ppac.designsystem.foundation.FarmemeIcon
 import team.ppac.sample.mvi.SampleIntent
 
 @Composable
@@ -39,6 +45,23 @@ fun SampleScreen(viewModel: SampleViewModel) {
                 text = "로딩중",
             )
         }
+        FarmemeCircleButton(
+            backgroundColor = FarmemeTheme.backgroundColor.assistive,
+            icon = { FarmemeIcon.Copy(Modifier.size(20.dp)) },
+        )
+
+        FarmemeWeakButton(
+            backgroundColor = FarmemeTheme.backgroundColor.assistive,
+            icon = { FarmemeIcon.Copy(Modifier.size(20.dp)) },
+            text = "버튼",
+            textColor = FarmemeTheme.textColor.primary,
+        )
+
+        FarmemeFilledButton(
+            backgroundColor = FarmemeTheme.backgroundColor.primary,
+            text = "버튼",
+            textColor = FarmemeTheme.textColor.inverse,
+        )
 
         LazyColumn {
             items(items = state.images) {
