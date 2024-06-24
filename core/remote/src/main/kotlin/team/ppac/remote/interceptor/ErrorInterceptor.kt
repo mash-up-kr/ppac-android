f5e526737fa3ea1091574a679f16a00fb4ca533c
evergreen <tkdfhr7497@naver.com>
2024-06-24 22:15:11 +0900

    [Refactor] @jasunglee 리뷰 반영

--- core/remote/src/main/kotlin/team/ppac/remote/interceptor/ErrorInterceptor.kt
+++ core/remote/src/main/kotlin/team/ppac/remote/interceptor/ErrorInterceptor.kt
@@ -6,6 +6,7 @@ import okhttp3.Response
 import team.ppac.errorhandling.FarmemeException
 import team.ppac.errorhandling.parseWithNetworkError
 import javax.inject.Inject
+import team.ppac.remote.model.response.Response as FarmemeResponse
 
 class ErrorInterceptor @Inject constructor(
     private val json: Json,
@@ -13,13 +14,13 @@ class ErrorInterceptor @Inject constructor(
     override fun intercept(chain: Interceptor.Chain): Response {
         try {
             val response = chain.proceed(chain.request())
-            if(response.isSuccessful){
+            if (response.isSuccessful) {
                 return response
             }
 
             //에러가 발생 하는 경우
             response.body?.let {
-                val errorBody = json.decodeFromString<team.ppac.remote.model.response.Response<*>>(it.string())
+                val errorBody = json.decodeFromString<FarmemeResponse<*>>(it.string())
                 throw FarmemeException(
                     code = errorBody.code,
                     message = errorBody.message
