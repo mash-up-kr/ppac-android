a1329b1e8a885e93176d3442a8ec6a4e0a635e85
Jaesung Lee <viki0114@naver.com>
2024-06-26 22:19:39 +0900

    [Feat] MainActivity edgeToEdge 활성화

--- app/src/main/java/team/ppac/MainActivity.kt
+++ app/src/main/java/team/ppac/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,9 +13,10 @@ import team.ppac.designsystem.component.scaffold.FarmemeScaffold
 import team.ppac.designsystem.foundation.FarmemeTheme
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity(){
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             FarmemeTheme {
