5272b41503075687894545868d1f7430554d46ee
ze-zeh <chojh922@gmail.com>
2024-06-30 15:11:07 +0900

    [Feat] MyPageLevelChip 추가

---
+++ feature/mypage/src/main/java/team/ppac/mypage/component/MyPageLevelChip.kt
@@ -0,0 +1,43 @@
+package team.ppac.mypage.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import team.ppac.designsystem.FarmemeTheme
+import team.ppac.designsystem.foundation.FarmemeRadius
+
+@Composable
+fun MyPageLevelChip(
+    modifier: Modifier = Modifier,
+    count: Int,
+) {
+    Row(
+        modifier = modifier
+            .clip(FarmemeRadius.Radius30.shape)
+            .background(FarmemeTheme.backgroundColor.white)
+            .padding(horizontal = 10.dp, vertical = 5.dp),
+    ) {
+        Text(
+            text = "$count",
+            color = FarmemeTheme.textColor.brand,
+            style = FarmemeTheme.typography.body.large.semibold,
+        )
+        Text(
+            text = "/20",
+            color = FarmemeTheme.textColor.tertiary,
+            style = FarmemeTheme.typography.body.large.semibold,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun MyPageLevelChipPreview() {
+    MyPageLevelChip(count = 10)
+}
