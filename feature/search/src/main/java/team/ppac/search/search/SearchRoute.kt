1dbf8e8cf1ee7a6c220121d075b30605d10ed1fb
Jaesung Lee <viki0114@naver.com>
2024-07-04 00:51:19 +0900

    [Feat] 검색 mvi 로직 Route로 이동

--- feature/search/src/main/java/team/ppac/search/search/SearchRoute.kt
+++ feature/search/src/main/java/team/ppac/search/search/SearchRoute.kt
@@ -1,15 +1,42 @@
 package team.ppac.search.search
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import team.ppac.search.search.component.OpenServiceDialog
+import team.ppac.search.search.mvi.SearchIntent
+import team.ppac.search.search.mvi.SearchSideEffect
 
 @Composable
 internal fun SearchRoute(
     modifier: Modifier = Modifier,
+    viewModel: SearchViewModel = hiltViewModel(),
+    navigateToSearchDetail: (String) -> Unit,
 ) {
+    val uiState by viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(key1 = viewModel) {
+        viewModel.sideEffect.collect { sideEffect ->
+            when (sideEffect) {
+                is SearchSideEffect.NavigateToSearchDetail -> navigateToSearchDetail(sideEffect.category)
+            }
+        }
+    }
+
+    if (uiState.showServiceOpenDialog) {
+        OpenServiceDialog(
+            onConfirmClick = { viewModel.showServiceOpenDialog(false) },
+            onDismiss = { viewModel.showServiceOpenDialog(false) }
+        )
+    }
+
     SearchScreen(
         modifier = modifier,
-        onCategoryClick = {},
-        navigateToSearchDetail = {}
+        uiState = uiState,
+        onSearchBarClick = { viewModel.intent(SearchIntent.ClickSearch(true)) },
+        onCategoryClick = { viewModel.intent(SearchIntent.ClickMemeCategory(category = it)) }
     )
 }
