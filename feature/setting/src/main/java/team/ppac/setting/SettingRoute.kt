5276db3f9cfc54d10cfd26dd0805562413ed11ee
ze-zeh <chojh922@gmail.com>
2024-07-03 19:40:20 +0900

    [Feat] Setting Navigation 추가

---
+++ feature/setting/src/main/java/team/ppac/setting/SettingRoute.kt
@@ -0,0 +1,11 @@
+package team.ppac.setting
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun SettingRoute(
+    modifier: Modifier = Modifier,
+) {
+    SettingScreen()
+}
