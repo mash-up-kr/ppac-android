f5e4ed744a02e3ad353e5cf55b64f33b41e83ca7
Jaesung Lee <viki0114@naver.com>
2024-07-08 01:12:00 +0900

    [Feat] 검색 결과 Grid Layout 생성

---
+++ feature/search/src/main/java/team/ppac/search/detail/component/SearchDetailResultContent.kt
@@ -0,0 +1,40 @@
+package team.ppac.search.detail.component
+
+import android.graphics.Bitmap
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.ImmutableList
+import team.ppac.common.android.component.FarmemeMemeItem
+import team.ppac.search.detail.model.SearchResultUiModel
+
+@Composable
+fun SearchDetailResultContent(
+    modifier: Modifier = Modifier,
+    searchResults: ImmutableList<SearchResultUiModel>,
+    onMemeClick: (String) -> Unit,
+    onCopyClick: (Bitmap?) -> Unit,
+) {
+    LazyVerticalStaggeredGrid(
+        modifier = modifier,
+        columns = StaggeredGridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(space = 12.dp),
+        verticalItemSpacing = 20.dp
+    ) {
+        items(items = searchResults) { uiModel ->
+            FarmemeMemeItem(
+                modifier = Modifier,
+                memeId = uiModel.memeId,
+                memeTitle = uiModel.memeTitle,
+                lolCount = uiModel.lolCount,
+                imageUrl = uiModel.imageUrl,
+                onMemeClick = onMemeClick,
+                onCopyClick = onCopyClick
+            )
+        }
+    }
+}
