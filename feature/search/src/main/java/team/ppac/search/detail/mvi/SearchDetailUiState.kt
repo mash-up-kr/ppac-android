fd13fdbd49b1d6cef46960e9e14a7fc2562788ad
Jaesung Lee <viki0114@naver.com>
2024-07-08 01:12:15 +0900

    [Feat] 검색 결과 스크린 및 초기 UI State 구성

--- feature/search/src/main/java/team/ppac/search/detail/mvi/SearchDetailUiState.kt
+++ feature/search/src/main/java/team/ppac/search/detail/mvi/SearchDetailUiState.kt
@@ -1,14 +1,62 @@
 package team.ppac.search.detail.mvi
 
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 import team.ppac.common.android.base.UiState
+import team.ppac.search.detail.model.SearchResultUiModel
 
 data class SearchDetailUiState(
     val memeCategory: String,
+    val searchResults: ImmutableList<SearchResultUiModel>,  // TODO(JaesungLeee) : 페이징 넣을 떄 수정 예정
 ) : UiState {
 
     companion object {
         val INITIAL_STATE = SearchDetailUiState(
-            memeCategory = ""
+            memeCategory = "",
+            searchResults = persistentListOf(
+                SearchResultUiModel(
+                    memeId = "asdf",
+                    imageUrl = "",
+                    memeTitle = "asdfasdf",
+                    lolCount = 0
+                ),
+                SearchResultUiModel(
+                    memeId = "asdf",
+                    imageUrl = "",
+                    memeTitle = "asdfasdf",
+                    lolCount = 10
+                ),
+                SearchResultUiModel(
+                    memeId = "asdf",
+                    imageUrl = "",
+                    memeTitle = "asdfasdfasdf",
+                    lolCount = 10
+                ),
+                SearchResultUiModel(
+                    memeId = "asdf",
+                    imageUrl = "",
+                    memeTitle = "asdfasdf",
+                    lolCount = 10
+                ),
+                SearchResultUiModel(
+                    memeId = "asdf",
+                    imageUrl = "",
+                    memeTitle = "asdfasdfasdf",
+                    lolCount = 10
+                ),
+                SearchResultUiModel(
+                    memeId = "asdf",
+                    imageUrl = "",
+                    memeTitle = "asdfasdf",
+                    lolCount = 10
+                ),
+                SearchResultUiModel(
+                    memeId = "asdf",
+                    imageUrl = "",
+                    memeTitle = "asdfasdfasdf",
+                    lolCount = 10
+                )
+            ),
         )
     }
 }
