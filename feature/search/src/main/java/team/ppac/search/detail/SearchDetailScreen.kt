05964affd2192a44e37aaeef16272da9408fb815
Jaesung Lee <viki0114@naver.com>
2024-07-08 01:12:15 +0900

    [Feat] 검색 결과 스크린 및 초기 UI State 구성

--- feature/search/src/main/java/team/ppac/search/detail/SearchDetailScreen.kt
+++ feature/search/src/main/java/team/ppac/search/detail/SearchDetailScreen.kt
@@ -1,13 +1,21 @@
 package team.ppac.search.detail
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import team.ppac.designsystem.component.scaffold.FarmemeScaffold
 import team.ppac.designsystem.component.toolbar.FarmemeBackToolBar
+import team.ppac.search.detail.component.SearchDetailResultContent
+import team.ppac.search.detail.component.SearchDetailResultHeader
 import team.ppac.search.detail.mvi.SearchDetailUiState
 
 @Composable
@@ -26,10 +34,31 @@ internal fun SearchDetailScreen(
             )
         }
     ) { paddingValues ->
-        Column(
-            modifier = Modifier.padding(paddingValues)
-        ) {
+        val innerPadding = PaddingValues(
+            top = paddingValues.calculateTopPadding(),
+            start = paddingValues.calculateStartPadding(LayoutDirection.Ltr),
+            end = paddingValues.calculateEndPadding(LayoutDirection.Ltr),
+            bottom = paddingValues.calculateBottomPadding() + 64.dp
+        )
 
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            SearchDetailResultHeader(totalCount = 10)
+            SearchDetailResultContent(
+                searchResults = uiState.searchResults,
+                onMemeClick = {},
+                onCopyClick = {}
+            )
         }
     }
+}
+
+@Preview
+@Composable
+private fun SearchDetailScreenPreview() {
+    SearchDetailScreen(
+        uiState = SearchDetailUiState.INITIAL_STATE,
+        navigateBack = {}
+    )
 }
