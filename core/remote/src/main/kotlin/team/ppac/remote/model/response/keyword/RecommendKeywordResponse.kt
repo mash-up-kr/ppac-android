6b77bc43f521b2207b9a6ab0085559aaa0f631ef
JaesungLeee <viki0114@naver.com>
2024-07-08 21:27:24 +0900

    [Feat] KeywordApi 및 DI 모듈 생성

---
+++ core/remote/src/main/kotlin/team/ppac/remote/model/response/keyword/RecommendKeywordResponse.kt
@@ -0,0 +1,12 @@
+package team.ppac.remote.model.response.keyword
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class RecommendKeywordResponse(
+    @field:Json(name = "keywords")
+    val recommendKeywords: List<String>,
+    @field:Json(name = "category")
+    val category: String,
+)
