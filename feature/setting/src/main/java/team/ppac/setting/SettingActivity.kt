09da6a8d689427c7e148e7cf91dabbcf35d40ee9
ze-zeh <chojh922@gmail.com>
2024-07-08 17:23:25 +0900

    [Feat] SettingActivity 추가

---
+++ feature/setting/src/main/java/team/ppac/setting/SettingActivity.kt
@@ -0,0 +1,17 @@
+package team.ppac.setting
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import team.ppac.designsystem.FarmemeTheme
+
+class SettingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FarmemeTheme {
+                SettingScreen()
+            }
+        }
+    }
+}
