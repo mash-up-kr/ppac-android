88e7830b92e743e09c84d44e7b87b3879df2f659
JaesungLeee <viki0114@naver.com>
2024-07-08 21:27:24 +0900

    [Feat] KeywordApi 및 DI 모듈 생성

---
+++ core/remote/src/main/kotlin/team/ppac/remote/api/KeywordApi.kt
@@ -0,0 +1,10 @@
+package team.ppac.remote.api
+
+import retrofit2.http.GET
+import team.ppac.remote.model.response.keyword.RecommendKeywordResponse
+
+internal interface KeywordApi {
+
+    @GET("api/keyword/recommend")
+    suspend fun getRecommendKeywords(): RecommendKeywordResponse
+}
