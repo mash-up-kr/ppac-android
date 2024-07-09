fbf02ebf438a0c1b221bcdafcd04c63c70853773
ze-zeh <chojh922@gmail.com>
2024-07-09 15:23:40 +0900

    [Refactor] SettingActivity에 AndroidEntryPoint 추가

--- feature/setting/src/main/java/team/ppac/setting/SettingActivity.kt
+++ feature/setting/src/main/java/team/ppac/setting/SettingActivity.kt
@@ -3,8 +3,10 @@ package team.ppac.setting
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import team.ppac.designsystem.FarmemeTheme
 
+@AndroidEntryPoint
 class SettingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
