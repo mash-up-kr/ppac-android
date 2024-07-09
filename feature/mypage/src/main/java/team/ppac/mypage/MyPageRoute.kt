ed341bc3c3f3888e75386418fb9a3f011333eb9c
ze-zeh <chojh922@gmail.com>
2024-07-10 00:17:03 +0900

    [Refactor] @EvergreenTree97 리뷰 반영 : 불필요한 Modifier 파라미터 삭제

--- feature/mypage/src/main/java/team/ppac/mypage/MyPageRoute.kt
+++ feature/mypage/src/main/java/team/ppac/mypage/MyPageRoute.kt
@@ -1,16 +1,13 @@
 package team.ppac.mypage
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 
 @Composable
 internal fun MyPageRoute(
-    modifier: Modifier = Modifier,
     navigateToDetail: () -> Unit,
     navigateToSetting: () -> Unit,
 ) {
     MyPageScreen(
-        modifier = modifier,
         navigateToDetail = navigateToDetail,
         navigateToSetting = navigateToSetting,
     )
