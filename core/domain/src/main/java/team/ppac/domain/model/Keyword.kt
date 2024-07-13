ad4dfb46bbe703a546488e1196803a5cee88cc25
Jaesung Lee <viki0114@naver.com>
2024-07-13 16:37:20 +0900

    [Feat] Keyword 도메인 모델 생성

--- core/domain/src/main/java/team/ppac/domain/model/Keyword.kt
+++ core/domain/src/main/java/team/ppac/domain/model/Keyword.kt
@@ -3,4 +3,9 @@ package team.ppac.domain.model
 data class Keyword(
     val id: String,
     val name: String,
+    val searchCount: Int?,
+    val createdAt: String?,
+    val updatedAt: String?,
+    val category: String?,
+    val imageUrl: String?,
 )
