e9b67bb508af7265f232cf035c3cf791e4c1ca59
Jaesung Lee <viki0114@naver.com>
2024-07-13 16:36:58 +0900

    [Feat] 인기 키워드 API Datasource 추가

--- core/remote/src/main/kotlin/team/ppac/remote/datasource/KeywordDataSource.kt
+++ core/remote/src/main/kotlin/team/ppac/remote/datasource/KeywordDataSource.kt
@@ -1,8 +1,9 @@
 package team.ppac.remote.datasource
 
 import team.ppac.remote.model.response.keyword.RecommendKeywordResponse
+import team.ppac.remote.model.response.keyword.TopKeywordResponse
 
 interface KeywordDataSource {
-
     suspend fun getRecommendKeywords(): List<RecommendKeywordResponse>
+    suspend fun getTopKeywords(): List<TopKeywordResponse>
 }
