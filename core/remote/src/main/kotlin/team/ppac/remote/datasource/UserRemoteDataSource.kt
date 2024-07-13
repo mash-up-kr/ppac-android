11e5693eee30f8a059f0eb17117b41beefd3dd98
ze-zeh <chojh922@gmail.com>
2024-07-13 14:41:45 +0900

    [Feat] UserRepository, UserRemoteDataSource에 최근 밈 조회, 나의 파밈함 구현

--- core/remote/src/main/kotlin/team/ppac/remote/datasource/UserRemoteDataSource.kt
+++ core/remote/src/main/kotlin/team/ppac/remote/datasource/UserRemoteDataSource.kt
@@ -1,8 +1,11 @@
 package team.ppac.remote.datasource
 
+import team.ppac.remote.model.response.meme.MemeResponse
 import team.ppac.remote.model.response.user.UserResponse
 
 interface UserRemoteDataSource {
     suspend fun postUser(deviceId: String): UserResponse
     suspend fun getUser(): UserResponse
+    suspend fun getUserSavedMemes(): List<MemeResponse>
+    suspend fun getUserRecentMemes(): List<MemeResponse>
 }
--- core/remote/src/main/kotlin/team/ppac/remote/datasource/impl/UserDataSourceImpl.kt
+++ core/remote/src/main/kotlin/team/ppac/remote/datasource/impl/UserDataSourceImpl.kt
@@ -3,6 +3,7 @@ package team.ppac.remote.datasource.impl
 import team.ppac.remote.api.UserApi
 import team.ppac.remote.datasource.UserRemoteDataSource
 import team.ppac.remote.model.request.user.PostUserRequest
+import team.ppac.remote.model.response.meme.MemeResponse
 import team.ppac.remote.model.response.user.UserResponse
 import javax.inject.Inject
 
@@ -15,5 +16,13 @@ internal class UserDataSourceImpl @Inject constructor(
 
     override suspend fun getUser(): UserResponse {
         return userApi.getUser()
+    }
+
+    override suspend fun getUserSavedMemes(): List<MemeResponse> {
+        return userApi.getUserSavedMemes()
+    }
+
+    override suspend fun getUserRecentMemes(): List<MemeResponse> {
+        return userApi.getUserRecentMemes()
     }
 }
