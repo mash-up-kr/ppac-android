75946db5ae200e9e2b60f65a88a9c6b1c00bb241
Jaesung Lee <viki0114@naver.com>
2024-07-13 16:36:58 +0900

    [Feat] 인기 키워드 API Datasource 추가

--- core/remote/src/main/kotlin/team/ppac/remote/api/KeywordApi.kt
+++ core/remote/src/main/kotlin/team/ppac/remote/api/KeywordApi.kt
@@ -1,10 +1,13 @@
 package team.ppac.remote.api
 
 import retrofit2.http.GET
+import team.ppac.remote.model.response.keyword.TopKeywordResponse
 import team.ppac.remote.model.response.keyword.RecommendKeywordResponse
 
 internal interface KeywordApi {
-
     @GET("api/keyword/recommend")
     suspend fun getRecommendKeywords(): List<RecommendKeywordResponse>
+
+    @GET("api/keyword/top")
+    suspend fun getTopKeywords(): List<TopKeywordResponse>
 }
