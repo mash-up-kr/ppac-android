da819ccc5ed56cead6f3550483c5160b73eb366b
hyejin12-ju <hyejin12.ju@samsung.com>
2024-07-14 22:56:35 +0900

    [Feature] detail bottombar 리플적용

--- feature/detail/src/main/java/team/ppac/detail/component/DetailBottomBar.kt
+++ feature/detail/src/main/java/team/ppac/detail/component/DetailBottomBar.kt
@@ -1,5 +1,7 @@
 package team.ppac.detail.component
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
@@ -9,21 +11,40 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import team.ppac.common.android.util.shareOneLink
 import team.ppac.designsystem.FarmemeTheme
 import team.ppac.designsystem.component.tabbar.TabBar
 import team.ppac.designsystem.foundation.FarmemeIcon
-import team.ppac.designsystem.util.extension.noRippleClickable
+import team.ppac.designsystem.foundation.FarmemeRadius
 
 @Composable
 internal fun DetailBottomBar(memeId: String) {
     val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+    val originalColor = FarmemeTheme.textColor.primary
+    val selectedColor = FarmemeTheme.textColor.brand
+
+    var copyButtonColor by remember { mutableStateOf(originalColor) }
+    var farmemeButtonChecked by remember { mutableStateOf(false) }
+    val farmemeButtonColor = if (farmemeButtonChecked) selectedColor else originalColor
+
     TabBar(modifier = Modifier.fillMaxWidth()) {
         Row(
             modifier = Modifier
@@ -33,9 +54,18 @@ internal fun DetailBottomBar(memeId: String) {
         ) {
             DetailBottomButton(
                 title = "복사",
-                onClickButton = {},
+                color = copyButtonColor,
+                onClickButton = {
+                    copyButtonColor = selectedColor
+                    coroutineScope.launch {
+                        delay(2000)
+                        copyButtonColor = originalColor
+                    }
+                },
             ) {
-                FarmemeIcon.Copy(modifier = Modifier.size(20.dp))
+                FarmemeIcon.Copy(
+                    modifier = Modifier.size(20.dp), tint = copyButtonColor
+                )
             }
             DetailBottomButton(
                 title = "공유",
@@ -44,10 +74,16 @@ internal fun DetailBottomBar(memeId: String) {
                 FarmemeIcon.Share(modifier = Modifier.size(20.dp))
             }
             DetailBottomButton(
-                title = "북마크",
-                onClickButton = {},
+                title = "파밈",
+                color = farmemeButtonColor,
+                onClickButton = {
+                    farmemeButtonChecked = !farmemeButtonChecked
+                },
             ) {
-                FarmemeIcon.BookmarkLine(modifier = Modifier.size(20.dp))
+                FarmemeIcon.BookmarkLine(
+                    modifier = Modifier.size(20.dp),
+                    tint = farmemeButtonColor,
+                )
             }
         }
     }
@@ -56,13 +92,19 @@ internal fun DetailBottomBar(memeId: String) {
 @Composable
 internal fun RowScope.DetailBottomButton(
     title: String,
+    color: Color = FarmemeTheme.textColor.primary,
     onClickButton: () -> Unit,
     icon: @Composable () -> Unit,
 ) {
     Row(
         modifier = Modifier
             .weight(1f)
-            .noRippleClickable(onClick = onClickButton)
+            .clip(shape = FarmemeRadius.Radius10.shape)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(color = FarmemeTheme.skeletonColor.primary),
+                onClick = { onClickButton() },
+            )
             .padding(vertical = 15.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
@@ -72,7 +114,7 @@ internal fun RowScope.DetailBottomButton(
         Text(
             text = title,
             style = FarmemeTheme.typography.body.xLarge.semibold,
-            color = FarmemeTheme.textColor.primary,
+            color = color,
         )
     }
 }
