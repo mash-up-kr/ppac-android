589cacc4461589d31e95b791b693efdff0698a73
ze-zeh <chojh922@gmail.com>
2024-07-19 17:57:28 +0900

    [Refactor] 불필요 줄바꿈 제거

--- feature/mypage/src/main/java/team/ppac/mypage/mvi/MyPageIntent.kt
+++ feature/mypage/src/main/java/team/ppac/mypage/mvi/MyPageIntent.kt
@@ -5,7 +5,6 @@ import team.ppac.common.android.base.UiIntent
 sealed class MyPageIntent : UiIntent {
     data object ClickSettingButton : MyPageIntent()
     data class ClickRecentMemeItem(val memeId: String) : MyPageIntent()
-
     data class ClickSavedMemeItem(val memeId: String) : MyPageIntent()
     data object RefreshData : MyPageIntent()
 }
