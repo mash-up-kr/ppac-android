56822c0ee2fa4c84559e6a32cf6f6d18ef21cc22
Jaesung Lee <viki0114@naver.com>
2024-07-21 23:47:56 +0900

    [Feat] 페이징 데이터 UI 표시

--- feature/search/src/main/java/team/ppac/search/detail/mvi/SearchDetailUiState.kt
+++ feature/search/src/main/java/team/ppac/search/detail/mvi/SearchDetailUiState.kt
@@ -1,62 +1,20 @@
 package team.ppac.search.detail.mvi
 
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.persistentListOf
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import team.ppac.common.android.base.UiState
 import team.ppac.search.detail.model.SearchResultUiModel
 
 data class SearchDetailUiState(
     val memeCategory: String,
-    val searchResults: ImmutableList<SearchResultUiModel>,  // TODO(JaesungLeee) : 페이징 넣을 떄 수정 예정
+    val searchResults: Flow<PagingData<SearchResultUiModel>>,
 ) : UiState {
 
     companion object {
         val INITIAL_STATE = SearchDetailUiState(
             memeCategory = "",
-            searchResults = persistentListOf(
-                SearchResultUiModel(
-                    memeId = "asdf",
-                    imageUrl = "",
-                    memeTitle = "asdfasdf",
-                    lolCount = 0
-                ),
-                SearchResultUiModel(
-                    memeId = "asdf",
-                    imageUrl = "",
-                    memeTitle = "asdfasdf",
-                    lolCount = 10
-                ),
-                SearchResultUiModel(
-                    memeId = "asdf",
-                    imageUrl = "",
-                    memeTitle = "asdfasdfasdf",
-                    lolCount = 10
-                ),
-                SearchResultUiModel(
-                    memeId = "asdf",
-                    imageUrl = "",
-                    memeTitle = "asdfasdf",
-                    lolCount = 10
-                ),
-                SearchResultUiModel(
-                    memeId = "asdf",
-                    imageUrl = "",
-                    memeTitle = "asdfasdfasdf",
-                    lolCount = 10
-                ),
-                SearchResultUiModel(
-                    memeId = "asdf",
-                    imageUrl = "",
-                    memeTitle = "asdfasdf",
-                    lolCount = 10
-                ),
-                SearchResultUiModel(
-                    memeId = "asdf",
-                    imageUrl = "",
-                    memeTitle = "asdfasdfasdf",
-                    lolCount = 10
-                )
-            ),
+            searchResults = flowOf(PagingData.empty())
         )
     }
 }
