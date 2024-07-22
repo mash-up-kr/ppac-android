c9dbbc1a4607ba9e9d11c5532ef26324b4d6edbf
JaesungLeee <viki0114@naver.com>
2024-07-22 12:03:08 +0900

    [Feat] 검색 결과 페이징 시 표시되는 Footer 컴포저블 추가

---
+++ feature/search/src/main/java/team/ppac/search/detail/component/SearchDetailResultFooter.kt
@@ -0,0 +1,38 @@
+package team.ppac.search.detail.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import team.ppac.designsystem.FarmemeTheme
+
+@Composable
+fun SearchDetailResultFooter(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 100.dp),
+        text = "카페인 빨리 충전하고\n재밌는 밈 더 찾아둘게요!",
+        style = FarmemeTheme.typography.body.large.medium.copy(
+            color = FarmemeTheme.textColor.assistive
+        ),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview
+@Composable
+private fun SearchDetailResultFooterPreview() {
+    Box(modifier = Modifier.background(Color.White)) {
+        SearchDetailResultFooter()
+    }
+}
