09984edf64ea0e1df9c10268e5fcab8628663542
JaesungLeee <viki0114@naver.com>
2024-07-23 17:05:05 +0900

    [Feat] 검색 상세 Intent, SideEffect 정의

--- feature/search/src/main/java/team/ppac/search/detail/mvi/SearchDetailIntent.kt
+++ feature/search/src/main/java/team/ppac/search/detail/mvi/SearchDetailIntent.kt
@@ -2,4 +2,8 @@ package team.ppac.search.detail.mvi
 
 import team.ppac.common.android.base.UiIntent
 
-sealed class SearchDetailIntent : UiIntent
+sealed class SearchDetailIntent : UiIntent {
+    data class ClickMeme(
+        val memeId: String,
+    ) : SearchDetailIntent()
+}
