ab8a639a3cbe7a86ec5dc6e5067f85941ab09b14
Jaesung Lee <viki0114@naver.com>
2024-07-26 22:36:43 +0900

    [Feat] TopBar 수정

--- core/designsystem/src/main/kotlin/team/ppac/designsystem/component/toolbar/Toolbar.kt
+++ core/designsystem/src/main/kotlin/team/ppac/designsystem/component/toolbar/Toolbar.kt
@@ -8,17 +8,16 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsIgnoringVisibility
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -64,14 +63,15 @@ internal fun FarmemeToolbar(
         Row(
             modifier = Modifier
                 .padding(
-                    top = with(LocalDensity.current) {
-                        WindowInsets.statusBarsIgnoringVisibility
-                            .getTop(this)
-                            .toDp()
-                    }
+                    horizontal = 20.dp,
+                    vertical = 15.dp
                 )
-                .fillMaxWidth()
-                .height(50.dp),
+                .padding(
+                    top = WindowInsets.statusBars
+                        .asPaddingValues()
+                        .calculateTopPadding()
+                )
+                .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Box(
