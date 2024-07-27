d23f3dc7f8e897c752ed08d603bb85adf882c991
ze-zeh <chojh922@gmail.com>
2024-07-27 14:53:26 +0900

    [Refactor] 문구 수정

--- feature/mypage/src/main/java/team/ppac/mypage/component/MyPageLevelBox.kt
+++ feature/mypage/src/main/java/team/ppac/mypage/component/MyPageLevelBox.kt
@@ -85,8 +85,8 @@ private fun MyPageLevelTitle(
     ) {
         Text(
             text = when (levelUiModel.myPageLevel) {
-                MyPageLevel.LEVEL4 -> "레벨업하고 싶다면"
-                else -> "최종 레벨 달성 미션"
+                MyPageLevel.LEVEL4 -> "최종 레벨 달성 미션"
+                else -> "레벨업하고 싶다면"
             },
             color = FarmemeTheme.textColor.tertiary,
             style = FarmemeTheme.typography.body.medium.semibold,
