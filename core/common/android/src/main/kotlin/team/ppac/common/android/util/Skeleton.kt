66aa88a97458c1ca2c2b476f75c580175b056af4
ze-zeh <chojh922@gmail.com>
2024-07-28 20:56:35 +0900

    [Feat] 스켈레톤 확장함수 수정

--- core/common/android/src/main/kotlin/team/ppac/common/android/util/Skeleton.kt
+++ core/common/android/src/main/kotlin/team/ppac/common/android/util/Skeleton.kt
@@ -12,55 +12,64 @@ import androidx.compose.ui.composed
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import team.ppac.designsystem.FarmemeTheme
 
-fun Modifier.shimmerLoadingAnimation(
+fun Modifier.showSkeleton(
     isLoading: Boolean = false,
-    durationMillis: Int = 1000,
+    viewType: SkeletonViewType = SkeletonViewType.Basic,
 ): Modifier {
-    val widthOfShadowBrush = 500
-    val angleOfAxisY = 270f
+    return if (!isLoading) {
+        this
+    } else {
+        composed {
+            val shadowWidth = with(LocalDensity.current) { 200.dp.toPx() }
+            val duration = 1000
+            val shimmerColors = when (viewType) {
+                SkeletonViewType.Basic -> listOf(
+                    FarmemeTheme.skeletonColor.primary,
+                    FarmemeTheme.backgroundColor.white,
+                    FarmemeTheme.skeletonColor.primary,
+                )
 
-    if (isLoading) {
-        return composed {
-            val color = FarmemeTheme.skeletonColor.primary
-            val shimmerColors = listOf(
-                color.copy(alpha = 0.3f),
-                color.copy(alpha = 0.5f),
-                color.copy(alpha = 1.0f),
-                color.copy(alpha = 0.5f),
-                color.copy(alpha = 0.3f),
-            )
+                SkeletonViewType.Home -> listOf(
+                    FarmemeTheme.backgroundColor.black.copy(alpha = 0.1f),
+                    FarmemeTheme.backgroundColor.brandSubAssistive.copy(alpha = 0.7f),
+                    FarmemeTheme.backgroundColor.black.copy(alpha = 0.1f),
+                )
+            }
 
             val transition = rememberInfiniteTransition(label = "")
-
             val translateAnimation = transition.animateFloat(
                 initialValue = 0f,
-                targetValue = (durationMillis + widthOfShadowBrush).toFloat(),
+                targetValue = duration + shadowWidth,
                 animationSpec = infiniteRepeatable(
                     animation = tween(
-                        durationMillis = durationMillis,
+                        durationMillis = duration,
                         easing = LinearEasing,
                     ),
                     repeatMode = RepeatMode.Restart,
                 ),
-                label = "Shimmer loading animation",
+                label = "",
             )
 
-            this.background(
+            background(
                 brush = Brush.linearGradient(
                     colors = shimmerColors,
-                    start = Offset(x = translateAnimation.value - widthOfShadowBrush, y = 0.0f),
-                    end = Offset(x = translateAnimation.value, y = angleOfAxisY),
+                    start = Offset(x = translateAnimation.value - shadowWidth, y = 0.0f),
+                    end = Offset(x = translateAnimation.value, y = 0.0f),
                 ),
             )
         }
-    } else {
-        return this
     }
 }
 
 fun Modifier.visibility(isVisible: Boolean): Modifier {
     val alphaValue = if (isVisible) 1f else 0f
     return this then alpha(alpha = alphaValue)
+}
+
+enum class SkeletonViewType {
+    Basic, Home
 }
