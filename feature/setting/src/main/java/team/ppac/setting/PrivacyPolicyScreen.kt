7fba6c01fd54003f3af92080589c4eb6284ddd4f
ze-zeh <chojh922@gmail.com>
2024-07-29 14:38:59 +0900

    [Feat] PrivacyPolicyScreen 화면

--- feature/setting/src/main/java/team/ppac/setting/PrivacyPolicyScreen.kt
+++ feature/setting/src/main/java/team/ppac/setting/PrivacyPolicyScreen.kt
@@ -1,22 +1,47 @@
 package team.ppac.setting
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import team.ppac.designsystem.util.extension.noRippleClickable
+import androidx.compose.ui.unit.dp
+import team.ppac.designsystem.FarmemeTheme
+import team.ppac.designsystem.component.scaffold.FarmemeScaffold
+import team.ppac.designsystem.component.toolbar.FarmemeBackToolBar
 
 @Composable
 internal fun PrivacyPolicyScreen(
     modifier: Modifier = Modifier,
     navigateToBack: () -> Unit,
 ) {
-    Text(
-        modifier = modifier
-            .fillMaxSize()
-            .noRippleClickable {
-                navigateToBack()
-            },
-        text = "PrivacyPolicy"
-    )
+    FarmemeScaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            FarmemeBackToolBar(
+                title = "개인정보 처리방침",
+                onClickBackIcon = navigateToBack,
+            )
+        }
+    ) {
+        Column(
+            modifier = modifier.fillMaxSize()
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(FarmemeTheme.backgroundColor.assistive),
+            )
+            Spacer(
+                // 웹뷰 추가
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(FarmemeTheme.backgroundColor.brand),
+            )
+        }
+    }
 }
