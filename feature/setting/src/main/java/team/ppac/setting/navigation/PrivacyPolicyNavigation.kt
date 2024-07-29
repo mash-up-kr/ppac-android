1a2384f7e4e781e133854aa362b348ee1a19f850
ze-zeh <chojh922@gmail.com>
2024-07-29 14:28:14 +0900

    [Feat] Navigation 추가

---
+++ feature/setting/src/main/java/team/ppac/setting/navigation/PrivacyPolicyNavigation.kt
@@ -0,0 +1,22 @@
+package team.ppac.setting.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import team.ppac.setting.PrivacyPolicyRoute
+
+const val PRIVACY_POLICY_ROUTE = "privacy_policy"
+
+fun NavController.navigateToPrivacyPolicy() = navigate(PRIVACY_POLICY_ROUTE)
+
+fun NavGraphBuilder.privacyPolicyScreen(
+    navigateToBack: () -> Unit,
+) {
+    composable(
+        route = PRIVACY_POLICY_ROUTE,
+    ) {
+        PrivacyPolicyRoute(
+            navigateToBack = navigateToBack
+        )
+    }
+}
