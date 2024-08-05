df1ddedd5ea54d9ee200d8cc3f5b5079e40a5548
ze-zeh <chojh922@gmail.com>
2024-08-05 14:57:31 +0900

    [Feat] 스플래시 화면 변경

--- feature/splash/src/main/java/team/ppac/splash/SplashActivity.kt
+++ feature/splash/src/main/java/team/ppac/splash/SplashActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
+import team.ppac.designsystem.FarmemeTheme
 import team.ppac.navigator.MainNavigator
 import team.ppac.splash.mvi.SplashSideEffect
 import javax.inject.Inject
@@ -41,13 +44,19 @@ class SplashActivity : ComponentActivity() {
                 }
             }
 
-            Box(
+            Column(
                 modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
             ) {
                 Image(
                     painter = painterResource(id = team.ppac.designsystem.R.drawable.img_splash_logo),
                     contentDescription = null,
+                )
+                Text(
+                    text = "내가 찾던 밈을 파밍하다",
+                    style = FarmemeTheme.typography.body.large.medium,
+                    color = FarmemeTheme.textColor.primary,
                 )
             }
         }
