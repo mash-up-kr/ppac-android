b82ce9e89393019685e74f3f7f01d464906bc135
ze-zeh <chojh922@gmail.com>
2024-08-05 15:03:18 +0900

    [Feat] Spacer 추가

--- feature/splash/src/main/java/team/ppac/splash/SplashActivity.kt
+++ feature/splash/src/main/java/team/ppac/splash/SplashActivity.kt
@@ -7,12 +7,15 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import team.ppac.designsystem.FarmemeTheme
@@ -53,6 +56,7 @@ class SplashActivity : ComponentActivity() {
                     painter = painterResource(id = team.ppac.designsystem.R.drawable.img_splash_logo),
                     contentDescription = null,
                 )
+                Spacer(modifier = Modifier.height(10.dp))
                 Text(
                     text = "내가 찾던 밈을 파밍하다",
                     style = FarmemeTheme.typography.body.large.medium,
