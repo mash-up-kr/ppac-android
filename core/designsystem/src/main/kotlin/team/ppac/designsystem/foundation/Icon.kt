574b983e78d08677752808e0a023718c75462d4c
Jaesung Lee <viki0114@naver.com>
2024-08-11 15:38:14 +0900

    WIP

--- core/designsystem/src/main/kotlin/team/ppac/designsystem/foundation/Icon.kt
+++ core/designsystem/src/main/kotlin/team/ppac/designsystem/foundation/Icon.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import team.ppac.designsystem.FarmemeTheme
 import team.ppac.designsystem.R
 
 object FarmemeIcon {
