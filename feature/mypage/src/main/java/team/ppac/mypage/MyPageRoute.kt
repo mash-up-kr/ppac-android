24543f21e30789e5915e0df8777aacb1c20c6015
JaesungLeee <viki0114@naver.com>
2024-08-26 17:04:50 +0900

    [Feat] 마이 페이지 analytics 화면 추적 로깅 추가

--- feature/mypage/src/main/java/team/ppac/mypage/MyPageRoute.kt
+++ feature/mypage/src/main/java/team/ppac/mypage/MyPageRoute.kt
@@ -6,17 +6,32 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import team.ppac.analytics.AnalyticsHelper
+import team.ppac.analytics.type.ScreenType
 import team.ppac.common.android.base.BaseComposable
+import team.ppac.common.android.util.ComposableLifecycle
 import team.ppac.designsystem.foundation.FarmemeIcon
 import team.ppac.mypage.mvi.MyPageIntent
 import team.ppac.mypage.mvi.MyPageSideEffect
 
 @Composable
 internal fun MyPageRoute(
+    analyticsHelper: AnalyticsHelper,
     viewModel: MyPageViewModel = hiltViewModel(),
     navigateToDetail: (String) -> Unit,
     navigateToSetting: () -> Unit,
 ) {
+    ComposableLifecycle { _, event ->
+        when (event) {
+            Lifecycle.Event.ON_START -> {
+                analyticsHelper.reportScreen(ScreenType.MY_PAGE)
+            }
+
+            else -> {}
+        }
+    }
+
     BaseComposable(viewModel = viewModel) { uiState ->
         LaunchedEffect(key1 = viewModel) {
             viewModel.sideEffect.collect { sideEffect ->
