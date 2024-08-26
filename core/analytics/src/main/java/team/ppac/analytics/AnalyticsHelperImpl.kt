5ecf695b727e9aa429d884bb1b9f75fb3fa61be9
JaesungLeee <viki0114@naver.com>
2024-08-26 14:35:42 +0900

    [Feat] analytics 로깅 방식 정의

--- core/analytics/src/main/java/team/ppac/analytics/AnalyticsHelperImpl.kt
+++ core/analytics/src/main/java/team/ppac/analytics/AnalyticsHelperImpl.kt
@@ -1,13 +1,30 @@
 package team.ppac.analytics
 
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ParametersBuilder
+import com.google.firebase.analytics.logEvent
+import team.ppac.analytics.type.ActionType
+import team.ppac.analytics.type.ScreenType
 import javax.inject.Inject
 
 internal class AnalyticsHelperImpl @Inject constructor(
     private val analytics: FirebaseAnalytics
 ) : AnalyticsHelper {
 
-    override fun screen() {
+    override fun logScreen(screen: ScreenType) {
+        analytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_NAME, ScreenType.getTag(screen))
+        }
+    }
 
+    override fun logAction(
+        action: ActionType,
+        screen: ScreenType,
+        params: ParametersBuilder.() -> Unit
+    ) {
+        analytics.logEvent(action.getAction()) {
+            param("page", ScreenType.getTag(screen))
+            params()
+        }
     }
 }
