536bb5cfe881c69261224ab9f0eecdca881b50ba
Jaesung Lee <viki0114@naver.com>
2024-08-27 00:59:46 +0900

    [Feat] 검색 상세 analytics 로깅 추가 : 밈 복사, 밈 클릭

--- feature/search/src/main/java/team/ppac/search/detail/SearchDetailRoute.kt
+++ feature/search/src/main/java/team/ppac/search/detail/SearchDetailRoute.kt
@@ -6,11 +6,13 @@ import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import team.ppac.analytics.AnalyticsHelper
+import team.ppac.analytics.action.SearchDetailAction
 import team.ppac.analytics.type.ScreenType
 import team.ppac.common.android.base.BaseComposable
 import team.ppac.common.android.util.ComposableLifecycle
 import team.ppac.search.detail.mvi.SearchDetailIntent
 import team.ppac.search.detail.mvi.SearchDetailSideEffect
+import timber.log.Timber
 
 @Composable
 internal fun SearchDetailRoute(
@@ -45,8 +47,31 @@ internal fun SearchDetailRoute(
             uiState = uiState,
             handleLoadStates = viewModel::handleLoadErrorStates,
             onBackClick = navigateBack,
-            onMemeClick = { viewModel.intent(SearchDetailIntent.ClickMeme(it)) },
-            onRetryClick = { viewModel.intent(SearchDetailIntent.ClickErrorRetry) }
+            onMemeClick = { memeId ->
+                analyticsHelper.reportAction(
+                    action = SearchDetailAction.CLICK_MEME,
+                    screen = ScreenType.SEARCH_DETAIL,
+                    params = {
+                        param("keyword_name", uiState.keyword)
+                    }
+                )
+
+                viewModel.intent(SearchDetailIntent.ClickMeme(memeId))
+            },
+            onRetryClick = { viewModel.intent(SearchDetailIntent.ClickErrorRetry) },
+            onCopyClick = { memeId, memeTitle ->
+                analyticsHelper.reportAction(
+                    action = SearchDetailAction.CLICK_COPY,
+                    screen = ScreenType.SEARCH_DETAIL,
+                    params = {
+                        param("meme_id", memeId)
+                        param("meme_title", memeTitle)
+                    }
+                )
+            },
+            onScrollContent = {
+                Timber.e("LOADING")
+            }
         )
     }
 }
