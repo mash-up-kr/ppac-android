69d0fd81954fd8ce8ca482dc519c7c305cd58d87
JaesungLeee <viki0114@naver.com>
2024-08-26 09:13:03 +0900

    [Feat] analytics userId 설정 및 application 함수 분리

--- app/src/main/java/team/ppac/FarmemeApplication.kt
+++ app/src/main/java/team/ppac/FarmemeApplication.kt
@@ -1,8 +1,10 @@
 package team.ppac
 
 import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
 import dagger.hilt.android.HiltAndroidApp
 import team.ppac.crashlytics.logger.CrashlyticsLoggerTree
+import team.ppac.local.datasource.AppConfig
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -11,11 +13,18 @@ class FarmemeApplication : Application() {
 
     @Inject
     lateinit var crashlyticsLoggerTree: CrashlyticsLoggerTree
+
+    @Inject
+    lateinit var analytics: FirebaseAnalytics
+
+    @Inject
+    lateinit var appConfig: AppConfig
+
     override fun onCreate() {
         super.onCreate()
         initTimber()
-        AppsFlyerLibRequester.initSdk(this)
-        AppsFlyerLibRequester.logOneLinkState()
+        initAnalytics()
+        initAppsFlyer()
     }
 
     private fun initTimber() {
@@ -24,5 +33,15 @@ class FarmemeApplication : Application() {
         } else {
             Timber.plant(crashlyticsLoggerTree)
         }
+    }
+
+    private fun initAnalytics() {
+        val deviceId = appConfig.deviceId
+        analytics.setUserId(deviceId)
+    }
+
+    private fun initAppsFlyer() {
+        AppsFlyerLibRequester.initSdk(this)
+        AppsFlyerLibRequester.logOneLinkState()
     }
 }
