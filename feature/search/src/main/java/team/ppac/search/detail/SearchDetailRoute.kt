69f869fc445a5ac9e1b0dd19067832c60384e67c
JaesungLeee <viki0114@naver.com>
2024-08-26 17:03:37 +0900

    [Feat] 검색 상세 analytics 화면 추적 로깅 추가

--- feature/search/src/main/java/team/ppac/search/detail/SearchDetailRoute.kt
+++ feature/search/src/main/java/team/ppac/search/detail/SearchDetailRoute.kt
@@ -4,17 +4,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import team.ppac.analytics.AnalyticsHelper
+import team.ppac.analytics.type.ScreenType
 import team.ppac.common.android.base.BaseComposable
+import team.ppac.common.android.util.ComposableLifecycle
 import team.ppac.search.detail.mvi.SearchDetailIntent
 import team.ppac.search.detail.mvi.SearchDetailSideEffect
 
 @Composable
 internal fun SearchDetailRoute(
     modifier: Modifier = Modifier,
+    analyticsHelper: AnalyticsHelper,
     viewModel: SearchDetailViewModel = hiltViewModel(),
     navigateBack: () -> Unit,
     navigateToMemeDetail: (String) -> Unit,
 ) {
+    ComposableLifecycle { _, event ->
+        when (event) {
+            Lifecycle.Event.ON_START -> {
+                analyticsHelper.reportScreen(ScreenType.SEARCH_DETAIL)
+            }
+
+            else -> {}
+        }
+    }
+
     BaseComposable(viewModel = viewModel) { uiState ->
         LaunchedEffect(key1 = viewModel) {
             viewModel.sideEffect.collect { sideEffect ->
