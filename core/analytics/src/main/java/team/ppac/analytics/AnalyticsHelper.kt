aa03e8aac9c5dad49315a33ae01d6c0cea6c2d8e
JaesungLeee <viki0114@naver.com>
2024-08-26 14:35:42 +0900

    [Feat] analytics 로깅 방식 정의

--- core/analytics/src/main/java/team/ppac/analytics/AnalyticsHelper.kt
+++ core/analytics/src/main/java/team/ppac/analytics/AnalyticsHelper.kt
@@ -1,6 +1,15 @@
 package team.ppac.analytics
 
+import com.google.firebase.analytics.ParametersBuilder
+import team.ppac.analytics.type.ActionType
+import team.ppac.analytics.type.ScreenType
+
 interface AnalyticsHelper {
 
-    fun screen()
+    fun logScreen(screen: ScreenType)
+    fun logAction(
+        action: ActionType,
+        screen: ScreenType,
+        params: ParametersBuilder.() -> Unit = {}
+    )
 }
