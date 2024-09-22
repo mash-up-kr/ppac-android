d01e9d0d8e9d8f5d1fa0dedd325962e65dfc3f16
ze-zeh <chojh922@gmail.com>
2024-09-22 20:25:44 +0900

    [Feat] RegisterListHeader 추가

---
+++ feature/register/src/main/java/team/ppac/register/component/RegisterListHeader.kt
@@ -0,0 +1,43 @@
+package team.ppac.register.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import team.ppac.designsystem.FarmemeTheme
+import team.ppac.designsystem.foundation.FarmemeIcon
+
+@Composable
+fun RegisterListHeader(
+    modifier: Modifier = Modifier,
+    title: String,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp),
+    ) {
+        Text(
+            text = title,
+            style = FarmemeTheme.typography.body.xLarge.semibold.copy(
+                color = FarmemeTheme.textColor.primary,
+            ),
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        FarmemeIcon.Required()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RegisterListHeaderPreview() {
+    RegisterListHeader(
+        title = "밈의 제목"
+    )
+}
