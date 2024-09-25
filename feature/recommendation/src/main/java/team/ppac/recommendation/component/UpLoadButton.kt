fd6b40a7127ceb7f5da0419f4f6d678131f200bb
evergreen <tkdfhr7497@naver.com>
2024-09-25 23:29:24 +0900

    [Feat] 버튼 테두리 애니메이션

--- feature/recommendation/src/main/java/team/ppac/recommendation/component/UpLoadButton.kt
+++ feature/recommendation/src/main/java/team/ppac/recommendation/component/UpLoadButton.kt
@@ -1,7 +1,14 @@
 package team.ppac.recommendation.component
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -9,10 +16,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.unit.dp
 import team.ppac.designsystem.FarmemeTheme
 import team.ppac.designsystem.foundation.FarmemeIcon
@@ -24,43 +35,60 @@ internal fun UpLoadButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
 ) {
-    BaseButton(
-        modifier = modifier,
-        backgroundColor = FarmemeTheme.backgroundColor.primary,
-        text = "나도 밈 올리기",
-        textColor = FarmemeTheme.textColor.inverse,
-        icon = {
-            FarmemeIcon.UpLoad()
-        },
-        onClick = onClick
+    val transition = rememberInfiniteTransition(label = "transition")
+    val angle by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart,
+        ),
+        label = "angle animation"
     )
-}
-
-@Composable
-private fun BaseButton(
-    modifier: Modifier = Modifier,
-    backgroundColor: Color,
-    text: String,
-    textColor: Color,
-    onClick: () -> Unit = { },
-    icon: @Composable () -> Unit,
-) {
-    Row(
+    val brush = Brush.sweepGradient(
+        colors = buildList {
+            add(FarmemeTheme.backgroundColor.brand)
+            add(FarmemeTheme.backgroundColor.primary)
+        }
+    )
+    Box(
         modifier = modifier
-            .height(36.dp)
             .clip(FarmemeRadius.Radius10.shape)
-            .background(color = backgroundColor)
-            .noRippleClickable(onClick = onClick)
-            .padding(horizontal = 15.dp),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
+            .noRippleClickable(onClick = onClick),
+        contentAlignment = Alignment.Center
     ) {
-        icon()
-        Spacer(Modifier.width(4.dp))
-        Text(
-            text = text,
-            style = FarmemeTheme.typography.body.medium.semibold,
-            color = textColor,
-        )
+        Box(
+            modifier = Modifier
+                .clip(FarmemeRadius.Radius10.shape)
+                .padding(1.5.dp)
+                .drawWithContent {
+                    rotate(angle) {
+                        drawCircle(
+                            brush = brush,
+                            radius = size.width,
+                        )
+                    }
+                    drawContent()
+                },
+        ) {
+            Row(
+                modifier = Modifier
+                    .height(36.dp)
+                    .clip(FarmemeRadius.Radius10.shape)
+                    .background(color = FarmemeTheme.backgroundColor.primary)
+                    .padding(horizontal = 15.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                FarmemeIcon.UpLoad()
+                Spacer(Modifier.width(4.dp))
+                Text(
+                    text = "나도 밈 올리기",
+                    style = FarmemeTheme.typography.body.medium.semibold,
+                    color = FarmemeTheme.textColor.inverse,
+                )
+            }
+        }
     }
+
 }
