e2c639b47783f72cb688764860e25d1a3966a769
ze-zeh <chojh922@gmail.com>
2024-09-26 16:47:22 +0900

    [Feat] INITIAL_STATE 추가

--- feature/register/src/main/java/team/ppac/register/model/RegisterCategoryUiModel.kt
+++ feature/register/src/main/java/team/ppac/register/model/RegisterCategoryUiModel.kt
@@ -1,8 +1,14 @@
 package team.ppac.register.model
 
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 data class RegisterCategoryUiModel(
-    val category: String,
-    val keywords: ImmutableList<String>,
-)
+    val category: String = "",
+    val keywords: ImmutableList<String> = persistentListOf(),
+) {
+    companion object {
+        val INITIAL_STATE
+            get() = RegisterCategoryUiModel()
+    }
+}
